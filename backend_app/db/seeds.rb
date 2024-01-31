c16f53e63bc8985059806fe7b49663e4d0e32332
jerryho <jerryjp3914@gmail.com>
2024-01-31 16:23:05 +0800

    fix: add permission example for difer roles

--- backend_app/db/seeds.rb
+++ backend_app/db/seeds.rb
@@ -7,26 +7,34 @@ require_relative '../models/granted_permission'
 # Define the role descriptions
 role_descriptions = ['admin', 'teacher', 'staff', 'student']
 
-# Define permissions
-permissions = [
-  { name: 'accounts:view' },
-  { name: 'accounts:modify' }
-]
-
 # Iterate over the descriptions and create roles if they don't exist
 role_descriptions.each do |desc|
   Todo::Role.find_or_create(name: desc)
 end
 
-# Create permissions
-permissions.each do |perm|
-  Todo::Permission.find_or_create(perm)
-end
+# Define permissions with corresponding roles
+permissions = {
+  admin: [
+    { name: 'accounts:view' },
+    { name: 'accounts:modify' }
+  ],
+  teacher: [
+    { name: 'course:view' },
+    { name: 'course:modify' }
+  ]
+}
 
-# Assign 'View Account' and 'Modify Account' permissions to the 'admin' role
-admin_role = Todo::Role.find(name: 'admin')
+# Iterate over the permissions hash to create roles and assign permissions
+permissions.each do |role_name, perms|
+  # Find or create the role
+  role = Todo::Role.find_or_create(name: role_name.to_s)
 
-permissions.each do |perm|
-  permission = Todo::Permission.find(name: perm[:name])
-  Todo::GrantedPermission.find_or_create(role: admin_role, permission:)
+  # Iterate over each permission for the role
+  perms.each do |perm|
+    # Find or create the permission
+    permission = Todo::Permission.find_or_create(perm)
+
+    # Find or create the granted permission linking the role to the permission
+    Todo::GrantedPermission.find_or_create(role: role, permission: permission)
+  end
 end
