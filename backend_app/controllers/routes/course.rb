cc5af1acf199a0afd49ed421275adbd5a58093a1
Hsuan <lala092325@gmail.com>
2024-02-25 09:08:29 +0000

    fix: change location routing under course

--- backend_app/controllers/routes/course.rb
+++ backend_app/controllers/routes/course.rb
@@ -147,6 +147,89 @@ module Todo
               end
             end
 
+            r.on 'location' do
+              # GET api/course/:course_id/location/list_all
+              r.on 'list_all' do
+                r.get do
+                  locations = LocationService.list_all(requestor)
+                  response.status = 200
+                  { success: true, data: locations }.to_json
+                rescue LocationService::ForbiddenError => e
+                  response.status = 403
+                  { error: 'Forbidden', details: e.message }.to_json
+                end
+              end
+
+              r.on String do |location_id|
+
+                r.on do
+                  # GET api/course/:course_id/location/:id
+                  r.get do
+                    location = LocationService.get(requestor, location_id)
+                    response.status = 200
+                    { success: true, data: location }.to_json
+                  rescue LocationService::ForbiddenError => e
+                    response.status = 403
+                    { error: 'Forbidden', details: e.message }.to_json
+                  end
+                  # PUT api/course/:course_id/location/:id
+                  r.put do
+                    request_body = JSON.parse(r.body.read)
+
+                    LocationService.update(requestor, location_id, request_body)
+                    response.status = 200
+                    { success: true, message: 'Location updated'}.to_json
+                  rescue LocationService::LocationNotFoundError => e
+                    response.status = 404
+                    { error: 'Location not found', details: e.message }.to_json
+                  rescue LocationService::ForbiddenError => e
+                    response.status = 403
+                    { error: 'Forbidden', details: e.message }.to_json
+                  end
+
+                  # DELETE api/course/:course_id/location/:id
+                  r.delete do
+                    LocationService.remove(requestor, location_id)
+                    response.status = 200
+                    { success: true, message: 'Location deleted' }.to_json
+                  rescue LocationService::ForbiddenError => e
+                    response.status = 403
+                    { error: 'Forbidden', details: e.message }.to_json
+                  rescue JSON::ParserError => e
+                    response.status = 400
+                    { error: 'Invalid JSON', details: e.message }.to_json
+                  rescue Sequel::NoMatchingRow => e
+                    response.status = 404
+                    { error: 'Location not found', details: e.message }.to_json
+                  end
+                end
+              end
+
+              # GET api/course/:course_id/location
+              r.get do
+                locations = LocationService.list(requestor)
+                response.status = 200
+                { success: true, data: locations }.to_json
+              rescue LocationService::ForbiddenError => e
+                response.status = 403
+                { error: 'Forbidden', details: e.message }.to_json
+              end
+
+              # POST api/course/:course_id/location
+              r.post do
+                request_body = JSON.parse(r.body.read)
+                location = LocationService.create(requestor, request_body)
+                response.status = 201
+                { success: true, message: 'Location created', location_info: location.attributes }.to_json
+              rescue JSON::ParserError => e
+                response.status = 400
+                { error: 'Invalid JSON', details: e.message }.to_json
+              rescue LocationService::ForbiddenError => e
+                response.status = 403
+                { error: 'Forbidden', details: e.message }.to_json
+              end
+            end
+
             r.on do
               # GET api/course/:id
               r.get do
--- backend_app/db/migration/008_location_create.rb
+++ backend_app/db/migration/008_location_create.rb
@@ -7,11 +7,15 @@ Sequel.migration do
     # Create locations table
     create_table(:locations) do
       primary_key :id
+      foreign_key :course_id, :courses # Links location to a specific course
+
       String :name
       Float :latitude
       Float :longitude
       DateTime :created_at
       DateTime :updated_at
+
+      unique %i[latitude longitude]
     end
   end
 end
