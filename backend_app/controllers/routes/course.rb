678b0852dccdc679f32a92b05ada74c320d0cd1b
jerryho <jerryjp3914@gmail.com>
2024-04-02 07:38:52 +0000

    fearure: create GET`attendance/:event_id` api

--- backend_app/controllers/routes/course.rb
+++ backend_app/controllers/routes/course.rb
@@ -93,7 +93,17 @@ module Todo
                   { error: 'Forbidden', details: e.message }.to_json
                 end
               end
-
+              r.on String do |event_id|
+                # GET api/course/:course_id/attendance/:event_id
+                r.get do
+                  attendances = AttendanceService.list_by_event(requestor, course_id, event_id)
+                  response.status = 200
+                  { success: true, data: attendances }.to_json
+                rescue AttendanceService::ForbiddenError => e
+                  response.status = 403
+                  { error: 'Forbidden', details: e.message }.to_json
+                end
+              end
               # GET api/course/:course_id/attendance
               r.get do
                 attendances = AttendanceService.list(requestor, course_id)
