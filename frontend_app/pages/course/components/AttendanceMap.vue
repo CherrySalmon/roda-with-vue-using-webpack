572de2d2ab34cc9ae2c2f4c4a91657b4233818f0
Hsuan <lala092325@gmail.com>
2024-04-27 18:18:29 +0000

    feature: allow owner/TA/instructor to mark attendance

--- frontend_app/pages/course/components/AttendanceMap.vue
+++ frontend_app/pages/course/components/AttendanceMap.vue
@@ -1,20 +1,46 @@
 <template>
     <div id="map" style="height: 600px; width: 100%;"></div>
+
+    <el-table style="width: 100%" :data="enrollments">
+        <el-table-column type="index" width="50" />
+        <el-table-column width="70">
+            <template #default="scope">
+                <el-avatar shape="square" :size="40" :src="scope.row.account.avatar" />
+            </template>
+        </el-table-column>
+        <el-table-column prop="account.name" label="Name" width="180" />
+        <el-table-column prop="account.email" label="Email" />
+        <el-table-column label="Attendance" width="220">
+            <template #default="scope">
+                <div v-if="eventAttendances.some(attendance => attendance.account_id === scope.row.account.id)">
+                    <el-button type="success" disabled>Attendance Recorded</el-button>
+                </div>
+                <div v-else>
+                    <el-button type="info" @click="postAttendance(event, scope.row.account.id)">Mark
+                        Attendance</el-button>
+                </div>
+            </template>
+        </el-table-column>
+    </el-table>
 </template>
 
 <script>
+import axios from 'axios'
+import cookieManager from '../../../lib/cookieManager';
 
-  export default {
-    emits: [],
+export default {
+    emits: ['closeDialog'],
     props: {
-      eventAttendances: Array,
-      event: Object
+        eventAttendances: Array,
+        event: Object,
+        enrollments: Object,
     },
     data() {
         return {
         }
     },
     mounted() {
+        this.accountCredential = cookieManager.getCookie('account_credential');
         this.initMap();
     },
     methods: {
@@ -44,11 +70,40 @@
 
             this.eventAttendances.forEach(attendance => {
                 new google.maps.Marker({
-                position: { lat: attendance.latitude, lng: attendance.longitude },
-                map: map,
-                title: attendance.name,
+                    position: { lat: attendance.latitude, lng: attendance.longitude },
+                    map: map,
+                    title: attendance.name,
                 });
             });
+        },
+        postAttendance(event, accountId) {
+            console.log('Posting attendance', event, accountId);
+            const courseId = event.course_id;
+            axios.post(`/api/course/${courseId}/attendance`, {
+                account_id: accountId,
+                event_id: event.id,
+                name: event.name,
+            }, {
+                headers: {
+                    Authorization: `Bearer ${this.accountCredential}`,
+                }
+            })
+                .then(response => {
+                    console.log('Attendance recorded successfully', response.data);
+                    this.$emit('closeDialog');
+                    ElMessageBox.alert('Attendance recorded successfully', 'Success', {
+                        confirmButtonText: 'OK',
+                        type: 'success',
+                    })
+                })
+                .catch(error => {
+                    console.error('Error recording attendance', error);
+                    this.$emit('closeDialog');
+                    ElMessageBox.alert('Fail to record attendance', 'Warning', {
+                        confirmButtonText: 'OK',
+                        type: 'warning',
+                    })
+                });
         }
     }
 }
